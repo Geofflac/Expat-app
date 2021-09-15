68a37422726c9b933a5a5ed6553f125c1c701d4a
Geofflac <84672157+Geofflac@users.noreply.github.com>
2021-09-15 22:40:42 +0800

    Added a first hello API, API is running on port 3000 to make it different than the application

--- expats-app/server.js
+++ expats-app/server.js
@@ -2,6 +2,28 @@ const express = require("express");
 const { join } = require("path");
 const app = express();
 
+
+// ------ Geoffre 15/09 try to install API  ----
+// -------- Start ----------
+
+// This is to tell that the body of of post will be in JSON format and will transform in JS
+const { response, request } = require('express');
+server = express();
+// This is to tell that the body of of post will be in JSON format and will transform in JS
+server.use(express.json());
+
+router = express.Router();
+
+router.get('/apihello', (request, response) => {
+    response.status(200).send("Hello!");
+} );
+
+server.use(router);
+
+server.listen(3000, () => console.log("API running on port 3000"));
+
+// ------ End ------------
+
 // Serve static assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
 
@@ -15,5 +37,5 @@ app.get("/*", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-// Listen on port 3000
+// Listen on port 5500
 app.listen(5500, () => console.log("Application running on port 5500"));
